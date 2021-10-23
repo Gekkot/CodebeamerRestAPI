f6c26a17ed4b3982339bbb0aa809fffd158fc09e
Gekko <hellogekko@gmail.com>
2021-10-23 19:51:55 +0300

    fix header for auth interceptor.

--- src/main/java/com/gekkot/cb/rest/AuthenticationInterceptor.java
+++ src/main/java/com/gekkot/cb/rest/AuthenticationInterceptor.java
@@ -4,6 +4,8 @@ package com.gekkot.cb.rest;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.http.HttpHeaders;
+import org.apache.http.message.BasicHeader;
 
 import java.io.IOException;
 
@@ -14,11 +16,18 @@ public class AuthenticationInterceptor implements Interceptor {
     public AuthenticationInterceptor(String token) {
         this.authToken = token;
     }
+    private BasicHeader getAuthenticationHeader() {
+        return new BasicHeader(HttpHeaders.AUTHORIZATION, authToken);
+    }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request original = chain.request();
-        Request.Builder builder = original.newBuilder().header("Authorization", authToken);
+        BasicHeader authenticationHeader = getAuthenticationHeader();
+        Request.Builder builder = original
+                .newBuilder()
+                .header(authenticationHeader.getName(),authenticationHeader.getValue())
+                .method(original.method(), original.body());
         Request request = builder.build();
         return chain.proceed(request);
     }
