164d0428b28620fa3aaa3cf90a4abc4b8b78ca26
Gekko <hellogekko@gmail.com>
2021-10-28 01:05:39 +0300

    add base caller with rx

---
+++ src/main/java/com/gekkot/cb/rest/common/BaseRxCaller.java
@@ -0,0 +1,44 @@
+package com.gekkot.cb.rest.common;
+
+import com.gekkot.cb.rest.common.callback.IDataErrorCallback;
+import com.gekkot.cb.rest.common.callback.INetworkExceptionCallback;
+import com.gekkot.cb.rest.common.callback.IResultCallback;
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+import java.io.IOException;
+
+public abstract class BaseRxCaller<ResponseType> {
+
+    protected abstract Observable<ResponseType> getObservable();
+
+    public void doCall(IResultCallback<ResponseType> resultCallback,
+                       IDataErrorCallback dataErrorCallback,
+                       INetworkExceptionCallback networkExceptionCallback) {
+
+        getObservable()
+                .subscribeOn(Schedulers.io())
+                .retry(2)
+                .observeOn(Schedulers.newThread())
+                .subscribe(new DisposableObserver<ResponseType>() {
+
+                    @Override
+                    public void onNext(ResponseType responseType) {
+                        System.out.println("onNext");
+                        resultCallback.onGetValue(responseType);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("onError");
+                        networkExceptionCallback.onGetNetworkException(new IOException(throwable));
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete!");
+                    }
+                });
+    }
+}
