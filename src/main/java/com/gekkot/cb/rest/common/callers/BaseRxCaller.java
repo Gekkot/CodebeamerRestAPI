d82139bb2f036d67012ab36802f27c74ea01e6ff
Konstantin Sorokin <sorokink@solvo.ru>
2021-11-02 21:57:22 +0300

    add structured reactive caller - get response and convert to another type. base reactive caller used structured caller as parent class

--- src/main/java/com/gekkot/cb/rest/common/callers/BaseRxCaller.java
+++ src/main/java/com/gekkot/cb/rest/common/callers/BaseRxCaller.java
@@ -1,44 +1,13 @@
 package com.gekkot.cb.rest.common.callers;
 
-import com.gekkot.cb.rest.common.callback.IDataErrorCallback;
-import com.gekkot.cb.rest.common.callback.INetworkExceptionCallback;
-import com.gekkot.cb.rest.common.callback.IResultCallback;
-import io.reactivex.Observable;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
-
-import java.io.IOException;
-
-public abstract class BaseRxCaller<ResponseType> {
-
-    protected abstract Observable<ResponseType> getObservable();
-
-    public void doCall(IResultCallback<ResponseType> resultCallback,
-                       IDataErrorCallback dataErrorCallback,
-                       INetworkExceptionCallback networkExceptionCallback) {
-
-        getObservable()
-                .subscribeOn(Schedulers.io())
-                .retry(2)
-                .observeOn(Schedulers.newThread())
-                .subscribe(new DisposableObserver<ResponseType>() {
-
-                    @Override
-                    public void onNext(ResponseType responseType) {
-                        System.out.println("onNext");
-                        resultCallback.onGetValue(responseType);
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-                        System.out.println("onError");
-                        networkExceptionCallback.onGetNetworkException(new IOException(throwable));
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        System.out.println("onComplete!");
-                    }
-                });
+/**
+ * Base reactive caller generic.
+ * Return response as if
+ * @param <ResponseType> response type
+ */
+public abstract class BaseRxCaller<ResponseType> extends StructuredRXCaller<ResponseType,ResponseType> {
+    @Override
+    protected ResponseType getAnswer(ResponseType responseType) {
+        return responseType;
     }
 }
---
+++ src/main/java/com/gekkot/cb/rest/common/callers/StructuredRXCaller.java
@@ -0,0 +1,52 @@
+package com.gekkot.cb.rest.common.callers;
+
+import com.gekkot.cb.rest.common.callback.IDataErrorCallback;
+import com.gekkot.cb.rest.common.callback.INetworkExceptionCallback;
+import com.gekkot.cb.rest.common.callback.IResultCallback;
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+import java.io.IOException;
+
+
+/**
+ * Structured generic reactive caller - get response and convert  it to another type
+ * @param <ResponseType> server response type
+ * @param <DataType> returning type
+ */
+public abstract class StructuredRXCaller<ResponseType, DataType> {
+
+    protected abstract Observable<ResponseType> getObservable();
+
+    protected abstract DataType getAnswer(ResponseType responseType);
+
+    public void doCall(IResultCallback<DataType> resultCallback,
+                       IDataErrorCallback dataErrorCallback,
+                       INetworkExceptionCallback networkExceptionCallback) {
+
+        getObservable()
+                .subscribeOn(Schedulers.io())
+                .retry(2)
+                .observeOn(Schedulers.newThread())
+                .subscribe(new DisposableObserver<ResponseType>() {
+
+                    @Override
+                    public void onNext(ResponseType responseType) {
+                        System.out.println("onNext");
+                        resultCallback.onGetValue(getAnswer(responseType));
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("onError");
+                        networkExceptionCallback.onGetNetworkException(new IOException(throwable));
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete!");
+                    }
+                });
+    }
+}
