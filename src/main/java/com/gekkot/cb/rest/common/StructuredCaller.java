9fcfa9060abbe51fd92e36c77093a955d7b77861
Konstantin Sorokin <sorokink@solvo.ru>
2021-11-02 21:51:31 +0300

    add structured caller - get response and convert to another type. base caller user structured caller as parent class

---
+++ src/main/java/com/gekkot/cb/rest/common/StructuredCaller.java
@@ -0,0 +1,50 @@
+package com.gekkot.cb.rest.common;
+
+import com.gekkot.cb.rest.common.callback.IDataErrorCallback;
+import com.gekkot.cb.rest.common.callback.INetworkExceptionCallback;
+import com.gekkot.cb.rest.common.callback.IResultCallback;
+import com.gekkot.cb.rest.common.exception.DataError;
+import com.gekkot.cb.rest.common.exception.IncorrectAnswerException;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
+/**
+ * Structured generic caller - get response and convert  it to another type
+ * @param <ResponseType> server response type
+ * @param <DataType> returning type
+ */
+public abstract class StructuredCaller<ResponseType, DataType> {
+
+    protected abstract Call<ResponseType> getCall();
+    protected abstract DataType getAnswer(ResponseType responseType);
+
+    public void doCall(IResultCallback<DataType> resultCallback,
+                       IDataErrorCallback dataErrorCallback,
+                       INetworkExceptionCallback networkExceptionCallback) {
+        try {
+            Call<ResponseType> call = getCall();
+            Response<ResponseType> execute = call.execute();
+            if (execute.isSuccessful()) {
+                if (execute.body() != null) {
+                    ResponseType responseType = execute.body();
+                    resultCallback.onGetValue(getAnswer(responseType));
+
+                } else {
+                    IOException ioException = new IncorrectAnswerException(call.request().url().toString(),"response  body is null");
+                    networkExceptionCallback.onGetNetworkException(ioException);
+                }
+            }
+            else {
+                int code = execute.code();
+                String message = execute.message();
+
+                DataError dataError = new DataError(call.request().url().toString(),code,message);
+                dataErrorCallback.onGetDataError(dataError);
+            }
+        } catch (IOException e) {
+            networkExceptionCallback.onGetNetworkException(e);
+        }
+    }
+}
